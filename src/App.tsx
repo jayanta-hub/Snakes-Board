ecd965f8f03d3b32bd6f3eedabcb41e73a71bb1c
jayanta-hub <76052991+jayanta-hub@users.noreply.github.com>
2024-09-18 19:51:02 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const BOARD_SIZE = 10;
+const App: React.FC = () => {
+  /**
+   * Renders the game board as a 2D array of cells.
+   *
+   * @return {JSX.Element[]} An array of JSX elements representing the rows of the board.
+   */
+
+  const renderBoard = (): JSX.Element[] => {
+    const rows: JSX.Element[] = [];
+    let isRowReversed = false;
+    for (let rowIndex = BOARD_SIZE; rowIndex > 0; rowIndex--) {
+      const rowCells: JSX.Element[] = [];
+      for (let colIndex = 0; colIndex < BOARD_SIZE; colIndex++) {
+        const cellNumber =
+          rowIndex * BOARD_SIZE -
+          (!isRowReversed ? colIndex : BOARD_SIZE - colIndex - 1);
+        rowCells.push(renderCell(cellNumber));
+      }
+
+      rows.push(
+        <div key={rowIndex} className="row">
+          {rowCells}
+        </div>
+      );
+
+      isRowReversed = !isRowReversed;
+    }
+
+    return rows;
+  };
+
+  // Render each cell
+  const renderCell = (cellNumber: number) => {
+    return (
+      <div
+        key={cellNumber}
+        className="cell"
+        style={{
+          backgroundColor: cellNumber % 2 === 0 ? "#ffffff" : "#ff0000",
+        }}
+      >
+        {cellNumber}
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="board">{renderBoard()}</div>
     </div>
   );
-}
+};
 
 export default App;
